aa0840202e1718dd6eea4832473b9c309ac14b3b
Jack Xu <Jack_Xu@cable.comcast.com>
2014-12-05 18:51:49 -0500

    functon 2 object

--- func1.scala
+++ func1.scala
@@ -8,6 +8,26 @@ println(formatResult("increment1", 2, (x) => x+1))
 
 println('z'+ 1)
 
+var increment1 = (x: Int) => x + 1
+// var increment2 = _ + 1
+
+println(increment1(3))
+// println(increment2(3))
+
+// val lessThan = new Function2[Int, Int, Boolean] {
+//   def apply(a: Int, b: Int) = a < b
+// }
+
+// println('6')
+
+object Main extends App {
+  Console.println("Hello World: " + (args mkString ", "))
+}
+
+// var func2 = new Function2[] {
+//   def apply(a: Int, b: Int) = a < b
+// }
+
 // val f = (x: Int) => x + 1
 
 // println(f)
