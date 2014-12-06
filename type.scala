af0bbadafbfd902cf42c8f89a02064f0fbf151e9
Jack Xu <Jack_Xu@cable.comcast.com>
2014-12-06 09:35:03 -0500

    output stuff

--- type.scala
+++ type.scala
@@ -13,3 +13,21 @@ println(b(1, 2))
 
 def sum(a: Int, b: Int, c: Int) = a + b + c
 
+val c = sum _  // '_' is a placeholder for the entire param list
+
+// c: (Int, Int, Int) => Int = <function3>
+
+println(c(1, 2, 3))
+
+def printlist(args: String*) = for(arg <- args) println(arg)
+
+printlist("2", "jack")
+println(Array("2", "jack"))
+// println(Array("2", "jack"): _*)
+println("2", "jack")
+printlist(Array("2", "jack"): _*)
+
+
+Console.println('1')
+Console.out.println('1')
+Console.err.println('1')
