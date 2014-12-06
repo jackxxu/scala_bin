0dc9050023dad52434d68896f3aa442eb276ef5b
Jack Xu <Jack_Xu@cable.comcast.com>
2014-12-06 08:26:33 -0500

    add placeholder param

---
+++ type.scala
@@ -0,0 +1,11 @@
+// var a = Any
+
+println("a".toLowerCase)
+println(Int)
+println(Unit)
+// println(java.lang.String)
+val a = (x: Int) => x*2
+val b = (_: Int) > 0 && (_: Int) > 0
+
+println(b(1, -1))
+println(b(1, 2))
